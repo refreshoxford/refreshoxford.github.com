02ca5da6cdbb4a1a3d1140e3355f83419d76c610
George Hickman <george@ghickman.co.uk>
2012-01-13 10:27:18 +0000

    Make the counter a real counter.
    +1 to Jepeto.

---
+++ main.js
@@ -0,0 +1,8 @@
+jQuery(document).ready(function () {
+    var options = {
+        format: 'dHM',
+        layout: '{dn} {dl}, {hn} {hl}, and {mn} {ml}',
+        until: new Date(2012, 2-1, 11, 10)
+    }
+    $('#countdown p').countdown(options);
+});
